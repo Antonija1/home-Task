0a737f1c1855703fce4a9abe44a031e5f0a18acd
Antonija1 <t.onja78@mail.ru>
2015-02-12 00:02:56 +0200

    exercise1

---
+++ src/homework/hw2/exercise1/Main.java
@@ -0,0 +1,14 @@
+package homework.hw2.exercise1;
+
+
+/**
+ * Created by Tonja on 11.02.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        char b = 'f';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
