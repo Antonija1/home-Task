c11c02c3fd0625fd57feb4ab3022fef2db7e2489
Antonija1 <t.onja78@mail.ru>
2015-02-13 20:01:57 +0200

    exercise11

---
+++ src/homework/hw2/exercise11/Person.java
@@ -0,0 +1,19 @@
+package homework.hw2.exercise11;
+
+/**
+ * Created by Tonja on 13.02.2015.
+ */
+public class Person {
+        int age;
+        int weight;
+        int stature;
+        int yearofbirth;
+
+       public Person(int age, int weight, int stature,int yearofbirth ) {
+            this.age = age;
+            this.weight= weight;
+            this.stature = stature;
+            this.yearofbirth = yearofbirth;
+Person person = new Person (10, 40, 140,2009);
+    }
+}
