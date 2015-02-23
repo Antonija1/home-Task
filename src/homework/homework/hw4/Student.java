5139387cb95aaba91a2f9e37c7e9eb16e90d4a86
Antonija1 <t.onja78@mail.ru>
2015-02-23 19:37:53 +0200

    homework4

---
+++ src/homework/homework/hw4/Student.java
@@ -0,0 +1,19 @@
+package homework.homework.hw4;
+
+/**
+ * Created by Tonja on 22.02.2015.
+ */
+public class Student extends Person {
+    private String idnumber;
+    private double dpa;
+
+    public void setIdnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public void setDpa(double dpa) {
+        this.dpa = dpa;
+    }
+
+
+}
