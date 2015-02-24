47bb4f68f10a01baf288ff026746b37983a169e0
Antonija1 <t.onja78@mail.ru>
2015-02-25 01:18:38 +0200

    homework4

--- src/homework/homework/hw4/Student.java
+++ src/homework/homework/hw4/Student.java
@@ -4,8 +4,14 @@ package homework.homework.hw4;
  * Created by Tonja on 22.02.2015.
  */
 public class Student extends Person {
-    private String idnumber;
-    private double dpa;
+    public String idnumber;
+    public double dpa;
+
+    public Student(String name, int age, String gender, String idnumber, double dpa) {
+        super(name, age, gender);
+        this.idnumber = idnumber;
+        this.dpa = dpa;
+    }
 
     public void setIdnumber(String idnumber) {
         this.idnumber = idnumber;
@@ -15,5 +21,11 @@ public class Student extends Person {
         this.dpa = dpa;
     }
 
+    public String getIdnumber() {
+        return idnumber;
+    }
 
+    public double getDpa() {
+        return dpa;
+    }
 }
