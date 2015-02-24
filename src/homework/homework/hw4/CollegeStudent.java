b7d9ab2f5baf6d5326b44e8ecdf877348c245cb8
Antonija1 <t.onja78@mail.ru>
2015-02-25 01:20:33 +0200

    homework4

---
+++ src/homework/homework/hw4/CollegeStudent.java
@@ -0,0 +1,38 @@
+package homework.homework.hw4;
+
+/**
+ * Created by Tonja on 25.02.2015.
+ */
+public class CollegeStudent extends Student {
+    private String major;
+    private int year;
+
+
+    public CollegeStudent(String name, int age, String gender, String idnumber, double dpa, int year, String major) {
+        super(name, age, gender, idnumber, dpa);
+        this.year = year;
+        this.major = major;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.major + " " + this.year + ", " + this.dpa + " " + this.idnumber +   " " + this.age + "" +
+                " " + this.gender+ "  "+this.name;
+    }
+}
