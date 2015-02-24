9904030f832741dc0b23e8491f8bb600e75b7ce0
Antonija1 <t.onja78@mail.ru>
2015-02-25 01:24:08 +0200

    homework4

--- src/homework/homework/hw4/Person.java
+++ src/homework/homework/hw4/Person.java
@@ -4,19 +4,28 @@ package homework.homework.hw4;
  * Created by Tonja on 22.02.2015.
  */
 public class Person {
-    private String name;
-    private int age;
-    private String gender;
+    public String name;
+    public int age;
+    public String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 
     public void setName(String name) {
         this.name = name;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
+
     public String getName() {
         return name;
     }
