b71d0e97b91a1b83c1e153aa10357ead9091776a
Antonija1 <t.onja78@mail.ru>
2015-03-02 20:55:38 +0200

    homework5

---
+++ src/homework/hw5/FractionNumberClasss.java
@@ -0,0 +1,23 @@
+package homework.hw5;
+
+
+    /**
+     * Created by Tonja on 28.02.2015.
+     */
+     interface FractionNumber {
+        double doubleValue();
+
+
+
+    }
+     class FractionNumberClass implements FractionNumber {
+
+        public int compareTo(FractionNumber o) {
+            return 0;
+        }
+
+         @Override
+         public double doubleValue() {
+             return 0;
+         }
+     }
